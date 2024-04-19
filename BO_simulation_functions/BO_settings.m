977c51eec7f2e27bb25b14ac904554552415a62b
MiriamKirchhoff <122794820+MiriamKirchhoff@users.noreply.github.com>
2024-04-19 17:24:14 +0200

    inital commit

---
+++ BO_simulation_functions/BO_settings.m
@@ -0,0 +1,75 @@
+function init = BO_settings()
+% function init = BO_settings()
+% input dialogue for settings of BO simulation
+% 
+% INPUTS
+    %
+% OUTPUTS
+    % init      struct with fields containing settings
+        % initial_samples       
+
+
+
+prompt = {...
+    'Number of initial random samples',...
+    'Number of iterations'...
+    'Model type(s)'...
+    'Acquisition function(s)'...
+    'Plotting true/false'...
+    'Path for loading data'...
+    'Path for saving data'...
+    'Kernel type'...
+    'Subjects (space-separated)'...
+    'Repetitions of each model'...
+    };
+definput = {...
+    '10',...
+    '1000'...
+    'GP, bayesian regression'...
+    'KG, random'...
+    'true'...
+    '\simulation_data\Summary_data_allsubjects_complete.mat'...
+    '\Bayesian optimization simulation\Results'...
+    'periodicMK'...
+    '1 7 19 22 32'...
+    '1000'...
+    };
+dlgtitle = 'Settings for bayesian optimization script';
+dims = [1 50];
+
+answer = inputdlg(prompt,dlgtitle,dims,definput);
+ 
+% extract answers into response struct
+init.n_init_samples     = str2double(answer{1});
+init.n_iterations       = str2double(answer{2});
+init.plotting           = eval(answer{5});
+init.path_load          = answer{6};
+init.path_save          = answer{7};
+init.type_kernel        = answer{8};
+init.subjects           = str2num(answer{9});
+init.n_repetitions      = str2double(answer{10});
+
+init.type_model = {};
+if contains(answer{3}, 'GP','IgnoreCase',true)
+    init.type_model = [init.type_model, {'GP'}]; end
+if contains(answer{3}, 'simple regression','IgnoreCase',true)
+    init.type_model = [init.type_model, {'simple regression'}]; end
+if contains(answer{3}, 'bayesian regression','IgnoreCase',true)
+    init.type_model = [init.type_model, {'bayesian regression'}]; end
+
+init.type_acquisition   = {};
+if contains(answer{4}, 'KG','IgnoreCase',true)
+    init.type_acquisition = [init.type_acquisition, {'KG'}]; end
+if contains(answer{4}, 'grid','IgnoreCase',true)
+    init.type_acquisition = [init.type_acquisition, {'grid'}]; end
+if contains(answer{4}, 'random','IgnoreCase',true)
+    init.type_acquisition = [init.type_acquisition, {'random'}]; end
+if contains(answer{4}, 'EI','IgnoreCase',true)
+    init.type_acquisition = [init.type_acquisition, {'EI'}]; end
+
+
+init.formatSpec = '%03.0f';
+
+
+
+
---
+++ BO_simulation_main.m
@@ -0,0 +1,281 @@
+%% Simulation main script
+% 1D search
+% Code by Miriam Kirchhoff, main script and functions adapted from
+% (1) Aino Nieminen & Jukka Sarvas, Aalto University
+% (2) Martin Krasser (https://github.com/krasserm)
+% v08072022
+%
+% version   1.0, 19.04.2024
+% author    Miriam Kirchhoff
+% project   C2B
+
+clear all
+close all
+
+%% SETTINGS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+settings = BO_settings;
+tic
+
+%% INITIALIZATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Load data
+
+disp('Loading data')
+load(settings.path_load)
+disp('Loading data completed')
+current_datetime = strrep(datestr(now),':','_');
+
+%% Generate computation grid
+
+% number of potential stimulation targets
+init.T_N = 200;
+% min and max of stimulation targets
+init.T_min = -pi;
+init.T_max = pi;
+grid_number = 8;
+T = linspace(init.T_min, init.T_max, init.T_N);
+n = numel(T);
+iter_max = settings.n_iterations + 1;
+
+
+%% Initialize result arrays
+
+dim = ["reps", "iter", "acq", "mod", "sub"];
+simulation_results_opt_estimate = NaN(settings.n_repetitions, settings.n_iterations+ 1, length(settings.type_acquisition), length(settings.type_model), length(settings.subjects));
+simulation_results_error        = simulation_results_opt_estimate;
+
+
+%% Initialize figure
+
+if settings.plotting
+    online_plot = figure('Name','Measured Data', 'NumberTitle','off');
+end % if settings.plotting
+
+%% Calculate ground truth for simulation
+
+% iterate over subject datasets
+for subject_idx = 1:length(settings.subjects)
+    current_subject = settings.subjects(subject_idx);
+    % generate ground truth for computation grid
+    subject_data.phases = all_trials.phase(...
+        all_trials.participant == current_subject & ~all_trials.outlier_all);
+    subject_data.transformed_mep_amplitudes = all_trials.MEP_log(...
+        all_trials.participant == current_subject & ~all_trials.outlier_all)';
+    simulation_parameters.percentage_data = 0.1;          % Percentage of complete window to be considered
+    simulation_parameters.steps =   n;
+    simulation_parameters.wintype = 'winsize';
+    simulation_parameters.winsize = (init.T_max - init.T_min)*simulation_parameters.percentage_data;  % window size of simulation
+    simulation_parameters.limits =  [init.T_min, init.T_max];
+    ground_truth = simulation_ground_truth_slidewindow(simulation_parameters, subject_data, 0, false, false, false);
+    clear simulation_parameters subject_data
+
+
+    %% Start loops
+
+    % iterate over selected models
+    for model_idx = 1:length(settings.type_model)
+        current_model = settings.type_model{model_idx};
+
+        % iterate over selected acquisition functions
+        for acquisition_idx = 1:length(settings.type_acquisition)
+            current_acquisition = settings.type_acquisition{acquisition_idx};
+
+            % this for can be replaced by parallel processing using parfor
+            % if plotting == 0
+            for repetition_current = 1:settings.n_repetitions
+
+                fprintf(['start repetition %g/%g, acquisition %g/%g, ' ...
+                    'model %g/%g, subject %g/%g \n'], ...
+                    repetition_current, settings.n_repetitions, ...
+                    acquisition_idx, length(settings.type_acquisition),...
+                    model_idx, length(settings.type_model), ...
+                    subject_idx, length(settings.subjects))
+
+
+                %% DATA GENERATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+                %% Initialize
+
+                t = [];
+                X_ind = [];
+                acq = [];
+
+                % Get initial measurement locations X_ind
+
+                temp = floor(length(T)/settings.n_init_samples);
+
+                % select random point in first 1/n area
+                X_new = randi(temp);
+
+                % add equally spaced samples
+                for i = 1:settings.n_init_samples-1
+                    X_new = [X_new X_new(1) + i*temp];
+                end % for i = 1:settings.n_init_samples-1
+                % clear i temp
+
+                % Start simulation loop
+
+                for iteration = 1:iter_max
+
+                    %% Get simulated target values t
+
+                    t_new = [];
+                    % sample all required new datapoints
+                    for i = 1:length(X_new)
+                        t_new = [t_new normrnd(ground_truth.mean(X_new(i)), ...
+                            ground_truth.sd(X_new(i)))];
+                    end % for i = 1:length(X_new)
+
+                    % Append to data arrays
+                    t = [t t_new];
+                    X_ind = [X_ind X_new];
+                    X = T(X_ind);
+                    % clear t_new X_new
+
+
+                    %% FIT MODEL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+                    switch current_model
+                        case 'GP'
+                            if iteration == 1
+                                GP_fit = struct('postmean', [], 'postvar', [], ...
+                                    'postmeanX', [], 'kernel', settings.type_kernel);
+                            end
+                            [y, y_var, GP_fit] = BO_fit_GP(X_ind, t, T, GP_fit);
+
+                        case 'bayesian regression'
+                            % fit a bayesian linear regression
+                            [m_N, S_N, beta] = blr_regression(X', t');
+                            phi_test = [ones(size(T')), sin(T'), cos(T')];
+                            [y, y_var] = blr_posterior_predictive(phi_test, m_N, S_N, beta);
+                    end % switch current_model
+
+
+                    %% ACQUISITION FUNCTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+                    switch current_acquisition
+                        case 'KG'
+                            % Choose the next stimulation parameter with the knowledge gradient.
+                            % Works well in 1D, but in higher dimensional cases,
+                            % computationally cheaper acquisition function may be needed.
+                            switch current_model
+                                case 'GP'
+                                    [X_new,acq] = tms_nextpoint_kg(X,t',GP_fit.mu0,...
+                                        GP_fit.a0,GP_fit.a1,GP_fit.lam2,T,GP_fit.muz3,...
+                                        ones(n,1)*GP_fit.lam2(1),GP_fit.kernel);
+
+                                otherwise
+                                    [X_new,acq] = blr_nextpoint_kg(X', t', beta, m_N, S_N, T');
+
+                                    % if acquisition function is noncontinuous
+                                    if sum(isnan(acq))
+                                        X_new = randi(length(T));
+                                        acq = NaN(size(T));
+                                        disp('selecting random simulation target due to nonconvergence')
+                                    end
+                            end
+
+                        case 'grid'
+                            % grid search
+                            % add random offset
+                            if iteration == 1
+                                grid_def = round(1:length(T)/grid_number:length(T));
+                                grid_random_offset = randi(length(T) - grid_def(end)+1)-1;
+                                grid_random_startlocation = randi(grid_number);
+                            end % if isempty(grid_random_offset)
+                            X_new = grid_def(mod(length(X_ind) + grid_random_startlocation, grid_number)+1);
+
+                        case 'random'
+                            % random sampling
+                            X_new = randi(length(T));
+
+                        case 'EI'
+                            % Choose the next stimulation parameter with the expected improvement.
+                            [X_new,acq] = tms_nextpoint_ei(y, y_var, y_X);
+                    end
+
+                    %% Save data
+                    [~, temp] = max(y);
+                    simulation_results_opt_estimate(repetition_current, ...
+                        iteration, acquisition_idx, model_idx, subject_idx) = T(temp);
+                    [~, temp2] = max(ground_truth.mean);
+                    noncirc_error = abs(T(temp) - ground_truth.X(temp2));
+                    circ_error = min(noncirc_error, range(T)-noncirc_error);
+                    simulation_results_error(repetition_current, ...
+                        iteration, acquisition_idx, model_idx, subject_idx) = circ_error;
+                    
+                    %% ONLINE PLOTTING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                    % parfor must be replaced by for to run online plotting
+
+                    if settings.plotting
+                        % check if plot exists, else build it
+                        if ~sum(ismember(findall(0,'type','figure'),online_plot))
+                            online_plot = figure('Name','Measured Data',...
+                                'NumberTitle','off');
+                        end
+                        subplot(2,1,1)
+                        hold off
+                        % plot data points
+                        scatter(T(X_ind), t, 'filled', 'k', 'MarkerFaceAlpha', 0.2, 'Markeredgealpha', 0.2)
+                        xlim([-pi pi])
+                        hold on
+
+                        % plot ground truth
+                        plot(ground_truth.X, ground_truth.mean, '--k')
+                        Y_gt = [ground_truth.mean+ground_truth.sd, flip(ground_truth.mean-ground_truth.sd)];
+                        X_area = [ground_truth.X, flip(ground_truth.X)];
+                        fill(X_area, Y_gt, 'm', 'LineStyle', 'none', 'FaceColor', 'k', 'FaceAlpha', 0.1)
+
+                        % plot estimate
+                        plot(T,y, 'Color', [160 20 20]/255, 'LineWidth',2)
+                        Y = [(y+y_var)', flip(y-y_var)'];
+                        fill(X_area, Y, 'm', 'LineStyle', 'none', 'FaceColor', [160 20 20]/255, 'FaceAlpha', 0.1)
+
+                        % plot next data point
+                        xline(T(X_new), 'LineWidth',2)
+
+                        % plot acquisition function
+                        try
+                            plot(T,(acq*0.1-max(acq*0.1)+min(Y_gt)), ':k', 'LineWidth',2)
+                        end % if acq
+
+                        title(['Simulation fitting ' current_model ' with acq ' current_acquisition ': ' num2str(length(t)) ' samples'])
+                        xlabel('Phase [rad]')
+                        ylim([min(Y_gt) - range(Y_gt), max(Y_gt) + range(Y_gt)])
+                        grid on
+                        drawnow
+
+                        % plot error over iterations
+                        subplot(2,1,2)
+                        hold off
+                        measured_values = simulation_results_error(:, ...
+                            :, acquisition_idx, model_idx, subject_idx);
+                        plot(measured_values', 'LineWidth',1, 'Color', [0 0 0 0.2])
+                        hold on
+                        plot(mean(measured_values, 'omitnan'), '-o', 'LineWidth',2, 'Color', [160 20 20]/255, 'MarkerFaceColor',[160 20 20]/255)
+
+                        grid on
+                        ylabel('error score [rad]')
+                        xlabel('iteration')
+                        xlim([1 max(sum(~isnan(measured_values(1,:))),2)])
+                        ylim([0 pi])
+
+                    end % if settings.plotting
+                end % for iteration = 1:settings.n_iterations
+            end % for repetition_current = 1:settings.repetitions
+            clear i temp Y temp temp2 circ_error noncirc_error m_N S_N beta phi_test t_new X_new
+
+            % Intermediate save
+            save(['\Results\Intermediate_Results_' current_datetime], '-v7.3');
+        end % for acquisition_idx = 1:length(settings.type_acquisition)
+    end % for model_idx = 1:length(settings.type_model)
+end % subject_idx = 1:length(settings.subjects) % iterate over subject datasets
+
+%% SAVE DATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+toc
+save([path.save '\Results' current_datetime], '-v7.3');
+
+
+
+
